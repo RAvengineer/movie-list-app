8ebfd3ec3946f304ced3ca9cf5bdb88132b2ab68
Rahul Bera <42903856+RAvengineer@users.noreply.github.com>
2021-08-19 21:01:23 +0530

    constants.dart: Add kTextInputDecoration

---
+++ lib/constants.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+InputDecoration kTextInputDecoration = InputDecoration(
+  labelText: 'Label Text',
+  border: OutlineInputBorder(
+    borderRadius: BorderRadius.circular(10.0),
+  ),
+);
