c72906e0b3a41e45fcd44968ca462303feddd01e
Rahul Bera <42903856+RAvengineer@users.noreply.github.com>
2021-08-20 06:23:06 +0530

    home_screen.dart: Add listener to update the UI - The ListView will be rebuilt everytime the content of the `movies` box is altered - Add formatting for displaying the contents of the box in ListTiles

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:movie_list/models/movie_model.dart';
 import 'package:movie_list/screens/add_movie_screen.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
 class HomeScreen extends StatelessWidget {
   HomeScreen({Key? key}) : super(key: key);
   static const String route = '/';
-  final items = List<String>.generate(10000, (i) => "Item $i");
 
   @override
   Widget build(BuildContext context) {
@@ -31,13 +32,22 @@ class HomeScreen extends StatelessWidget {
             )
           },
         ),
-        body: ListView.builder(
-          itemCount: items.length,
-          itemBuilder: (context, index) {
-            return ListTile(
-              title: Text(items[index]),
-            );
-          },
+        body: ValueListenableBuilder(
+          valueListenable: Hive.box<Movie>('movies').listenable(),
+          builder: (context, Box<Movie> _movieBox, widget) => ListView.builder(
+            itemCount: _movieBox.length,
+            itemBuilder: (context, index) {
+              final movie = _movieBox.getAt(index);
+              return ListTile(
+                title: Text(movie!.name),
+                subtitle: Text(movie.directors.join(' | ')),
+                leading: Image.memory(
+                  movie.posterBytes,
+                  fit: BoxFit.contain,
+                ),
+              );
+            },
+          ),
         ),
       ),
     );
