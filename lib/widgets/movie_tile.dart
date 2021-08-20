ad158afe68d482cf792a429359a5cf540aae0dec
Rahul Bera <42903856+RAvengineer@users.noreply.github.com>
2021-08-20 10:49:03 +0530

    movie_tile.dart: Extract MovieTile - Add Slideable widget & it's actions - Add actions for Delete & Edit

---
+++ lib/widgets/movie_tile.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
+import 'package:movie_list/models/movie_model.dart';
+
+class MovieTile extends StatelessWidget {
+  const MovieTile({Key? key, required this.movie}) : super(key: key);
+  final Movie movie;
+
+  @override
+  Widget build(BuildContext context) {
+    return Slidable(
+      actionPane: SlidableStrechActionPane(),
+      actions: [
+        IconSlideAction(
+          caption: 'Delete',
+          icon: Icons.delete_outlined,
+          color: Colors.redAccent,
+          onTap: () => print('Delete'),
+        ),
+      ],
+      secondaryActions: [
+        IconSlideAction(
+          caption: 'Edit',
+          icon: Icons.mode_edit_outline_outlined,
+          color: Colors.purpleAccent,
+          onTap: () => print('Edit'),
+        ),
+      ],
+      child: ListTile(
+        title: Text(movie.name),
+        subtitle: Text(movie.directors.join(' | ')),
+        leading: movie.posterBytes.isEmpty
+            ? Image.asset('assets/images/poster-icon.png')
+            : Image.memory(
+                movie.posterBytes,
+                fit: BoxFit.contain,
+              ),
+        trailing: Icon(
+          movie.watched ? Icons.playlist_add_check : Icons.playlist_play,
+          color: movie.watched ? Colors.greenAccent : Colors.amber[700],
+          size: 32.0,
+        ),
+      ),
+    );
+  }
+}
